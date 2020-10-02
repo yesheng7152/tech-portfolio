8f9c24418a1e3cbae93f09114ce8f9772ac0fb80
yesheng7152 <32319394+yesheng7152@users.noreply.github.com>
2020-10-02 14:47:51 -0500

    solve typos

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Yesheng Chen', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist, deicated problem solver.',
+    tagline: 'Continuously improving, aspiring technologist, dedicated problem solver.',
     profilePhoto: 'img/profile.png',
     url: 'https://yesheng7152.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
